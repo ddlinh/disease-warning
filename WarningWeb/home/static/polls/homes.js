6c0201fe4df09f104d0840ea4da679dad092b9e4
Dương Quang Vinh <duongquangvinh6@gmail.com>
2020-12-13 16:06:38 +0700

    interface update

---
+++ WarningWeb/home/static/polls/homes.js
@@ -0,0 +1,16 @@
+function Filter() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("location");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("dropdown-search");
+    a = div.getElementsByTagName("a");
+    for(i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
